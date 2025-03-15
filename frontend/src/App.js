96c2a400a8551611910f1f76303828bb0e8473cc
answjddnjs04 <infinitefoever@naver.com>
2025-03-15 09:42:45 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BlockList from './components/BlockList';
+import BlockForm from './components/BlockForm';
+import BlockDetail from './components/BlockDetail';
+import './App.css';
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<BlockList />} />
+            <Route path="/create" element={<BlockForm />} />
+            <Route path="/edit/:id" element={<BlockForm />} />
+            <Route path="/block/:id" element={<BlockDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
