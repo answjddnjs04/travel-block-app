eb64b93194cd3ba736be279a4b8c55822b504a21
answjddnjs04 <infinitefoever@naver.com>
2025-03-15 15:17:30 +0900

    Update axiosConfig.js

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-// GitHub Codespaces에서의 백엔드 URL 설정
-const BACKEND_URL = 'https://crispy-space-pancake-pj9r5vpvxr4jc9w76-5001.app.github.dev';
-
+// 개발 환경에서 proxy 설정 활용
 const api = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: '', // 비워두면 package.json의 proxy 설정 사용
   headers: {
     'Content-Type': 'application/json'
   },
-  withCredentials: false // CORS 설정에 맞게 변경
+  withCredentials: true // CORS 이슈 해결을 위해 true로 설정
 });
 
 export default api;
