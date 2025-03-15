f735cad518289af5e82fc5622043d9dda536d9d7
answjddnjs04 <infinitefoever@naver.com>
2025-03-15 14:03:25 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001; // 포트를 5001로 변경
 
 // 미들웨어 설정
 app.use(cors());
