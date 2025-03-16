88424c3f4d8af64bc12bd3f13ae0cce37c565592
answjddnjs04 <infinitefoever@naver.com>
2025-03-16 09:46:26 +0900

    Update axiosConfig.js

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
-// 실제 백엔드 서버 URL
-const backendURL = 'https://crispy-space-pancake-pj9r5vpvxr4jc9w76-5001.app.github.dev';
-
-const api = axios.create({
-  baseURL: backendURL,
-  headers: {
-    'Content-Type': 'application/json'
+// 직접 더미 데이터를 반환하는 mock axios 인스턴스 생성
+const api = {
+  get: (url) => {
+    console.log('Mock API GET 호출:', url);
+    return Promise.resolve({ data: {} });
   },
-  withCredentials: true
-});
+  post: (url, data) => {
+    console.log('Mock API POST 호출:', url, data);
+    return Promise.resolve({ data: { ...data, _id: 'mock-id-' + Date.now() } });
+  },
+  put: (url, data) => {
+    console.log('Mock API PUT 호출:', url, data);
+    return Promise.resolve({ data });
+  },
+  delete: (url) => {
+    console.log('Mock API DELETE 호출:', url);
+    return Promise.resolve({ data: { message: '성공적으로 삭제되었습니다' } });
+  }
+};
 
 export default api;
