ff769a6123ef2f9cfa1e1058d7a0398766d9b6c8
answjddnjs04 <infinitefoever@naver.com>
2025-03-16 12:52:40 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,3 +17,21 @@ function App() {
         <div className="container">
           <Routes>
             {/* 블록 관련 라우트 */}
+            <Route path="/" element={<BlockList />} />
+            <Route path="/create" element={<BlockForm />} />
+            <Route path="/edit/:id" element={<BlockForm />} />
+            <Route path="/block/:id" element={<BlockDetail />} />
+            
+            {/* 여행 계획 관련 라우트 */}
+            <Route path="/plans" element={<PlanList />} />
+            <Route path="/plans/create" element={<PlanForm />} />
+            <Route path="/plans/edit/:id" element={<PlanForm />} />
+            <Route path="/plans/:id" element={<PlanDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
